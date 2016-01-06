896d9b4aa7626be925b090647ce63594f5108029
BBK-PiJ-2015-81 <abrait02@dcs.bbk.ac.uk>
2016-01-06 20:55:40 +0000

    Started blocking out MeetingImpl so it compiles.

---
+++ MeetingImpl.java
@@ -0,0 +1,21 @@
+
+
+/**
+ * Created by andre on 06/01/2016.
+ */
+
+import java.util.Calendar;
+import java.util.Set;
+
+public abstract class MeetingImpl implements Meeting, java.io.Serializable   {
+
+    private int ID;
+    private Calendar date;
+    private Set<Contact> contacts;
+
+    //The Constructor
+    public Meeting
+
+
+
+}
