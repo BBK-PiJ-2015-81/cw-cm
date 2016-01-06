ff9e9ef6f37ad5bffb0eb20dcd234c0bf3fb630e
BBK-PiJ-2015-81 <abrait02@dcs.bbk.ac.uk>
2016-01-06 02:40:30 +0000

    Added exception handling to ContactImpl.

--- ContactImpl.java
+++ ContactImpl.java
@@ -8,10 +8,33 @@ public class ContactImpl implements Contact, java.io.Serializable {
     private String notes;
 
     //Three parameter constructor
-    public ContactImpl(int ID, String name, String notes)    {
-        this.ID = ID;
-        this.name = name;
-        this.notes = notes;
+    public ContactImpl(int ID, String name, String notes) {
+
+        if (ID <= 0) {
+            throw new IllegalArgumentException("Exception! ID was 0 or less.");
+        } else if (name == null) {
+            throw new NullPointerException("Exception! A name was not provided.");
+        } else if (notes == null) {
+            throw new NullPointerException("Exception! A note was not provided.");
+        } else {
+            this.ID = ID;
+            this.name = name;
+            this.notes = notes;
+        }
+    }
+
+    //Two parameter constructor
+    public ContactImpl(int ID, String name) {
+
+        if (ID <= 0) {
+            throw new IllegalArgumentException("Exception! ID was 0 or less.");
+        } else if (name == null) {
+            throw new NullPointerException("Exception! A name was not provided.");
+        } else {
+            this.ID = ID;
+            this.name = name;
+            this.notes = "";
+        }
     }
 
     public int getId() {
@@ -27,10 +50,15 @@ public class ContactImpl implements Contact, java.io.Serializable {
     }
 
     public void addNotes(String note) {
-        if (getNotes().equals(""))  {
-            notes = note;
-        } else {
-            notes = notes + "\n" + note;
+
+        // Don't allow adding null or an empty string to the notes
+        if ((note != null) && !(note.equals(""))) {
+
+            if (getNotes().equals("")) {
+                notes = note;
+            } else {
+                notes = notes + "\n" + note;
+            }
         }
     }
 }
--- ContactTest.java
+++ ContactTest.java
@@ -36,9 +36,57 @@ public class ContactTest {
     }
 
     @Test
-    public void testingaddNotes() {
-        testContact.addNotes("Really!");
-        assertEquals(expectedNotes + "\n" + "Really!", testContact.getNotes());
+    public void testingAddNotes() {
+        testContact.addNotes("Really?!");
+        assertEquals(expectedNotes + "\n" + "Really?!", testContact.getNotes());
+        //System.out.println(testContact.getNotes());
     }
 
+    @Test
+    public void testingAddNotesEmptyString() {
+        testContact.addNotes("");
+        assertEquals(expectedNotes, testContact.getNotes());
+    }
+
+    @Test
+    public void testingAddNotesNull() {
+        testContact.addNotes(null);
+        assertEquals(expectedNotes, testContact.getNotes());
+    }
+
+    @Test (expected = IllegalArgumentException.class)
+    public void testingIdThrowsException() {
+        testContact = new ContactImpl(-1, expectedName, expectedNotes);
+    }
+
+    @Test (expected = NullPointerException.class)
+    public void testingNameThrowsException() {
+        testContact = new ContactImpl(expectedID, null, expectedNotes);
+    }
+
+    @Test (expected = NullPointerException.class)
+    public void testingNotesThrowsException() {
+        testContact = new ContactImpl(expectedID, expectedName, null);
+    }
+
+    @Test (expected = IllegalArgumentException.class)
+    public void testingAllThrowException() {
+        testContact = new ContactImpl(-1, null, null);
+    }
+
+    @Test (expected = IllegalArgumentException.class)
+    public void testingIdThrowsException2param() {
+        testContact = new ContactImpl(-1, expectedName);
+    }
+
+    @Test (expected = NullPointerException.class)
+    public void testingNameThrowsException2param() {
+        testContact = new ContactImpl(expectedID, null);
+    }
+
+    @Test
+    public void testingGetNotes2param() {
+        testContact = new ContactImpl(expectedID, expectedName);
+        assertEquals("", testContact.getNotes());
+    }
 }
