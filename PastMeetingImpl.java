0e3f3f8bda1f808fbb61b1ca27719a3cf910cc64
BBK-PiJ-2015-81 <abrait02@dcs.bbk.ac.uk>
2016-01-07 19:28:17 +0000

    Added fourth parameter to PastMeetingImpl Constructor.

--- PastMeetingImpl.java
+++ PastMeetingImpl.java
@@ -10,7 +10,7 @@ public class PastMeetingImpl extends MeetingImpl implements PastMeeting {
     private String notes;
 
     //Call MeetingImpl's constructor with arguments the PastMeetingImpl constructor takes.
-    public PastMeetingImpl(int ID, Calendar date, Set<Contact> contacts){
+    public PastMeetingImpl(int ID, Calendar date, Set<Contact> contacts, String notes){
         super(ID, date, contacts);
     }
 
