69f6cf5f46985b81eacc74358006e713586e3610
BBK-PiJ-2015-81 <abrait02@dcs.bbk.ac.uk>
2016-01-11 09:21:19 +0000

    Started ContactManager tests

--- ContactManagerTest.java
+++ ContactManagerTest.java
@@ -3,22 +3,45 @@
  */
 
 import org.junit.*;
+import static org.junit.Assert.*;
 import java.util.*;
 import java.io.*;
 import java.nio.file.*;
 
 public class ContactManagerTest {
 
-
+    private ContactManager myManager;
+    private ContactManager myEmptyManager;
 
 
     @Before
     public void setUp() {
-        /*expectedID = 1;
-        expectedName = "Donald Trump";
-        expectedNotes = "Running for president";
-        testContact = new ContactImpl(expectedID, expectedName, expectedNotes);
-        */
+
+        // Create some contact managers
+        ContactManager myManager = new ContactManagerImpl();
+        ContactManager myEmptyManager = new ContactManagerImpl();
+
+        // Add some contacts
+
+        myManager.addNewContact("Manuel Neuer", "Goalkeeper");
+        myManager.addNewContact("Philipp Lahm", "Right back");
+        myManager.addNewContact("David Luiz", "Right centre back");
+        myManager.addNewContact("Sergio Ramos", "Left centre back");
+        myManager.addNewContact("Thiago Silva", "Left back");
+        myManager.addNewContact("Andres Iniesta", "Right midfield");
+        myManager.addNewContact("Toni Kroos", "Centre midfield");
+        myManager.addNewContact("Angel Di Maria", "Left midfield");
+        myManager.addNewContact("Lionel Messi", "Right wing");
+        myManager.addNewContact("Cristiano Ronaldo", "Centre forward");
+        myManager.addNewContact("Arjen Robben", "Left wing");
+
+    }
+
+    // Test Constructor
+
+    @Test
+    public void testingConstructor() {
+        assertNotNull(myEmptyManager);
     }
 
 
